9754ef571bfd200858feea1d69730431aa20561f
Altaluna <federiconj@gmail.com>
2025-09-29 16:09:33 -0300

    aaaa

--- servilink-pro/src/context/AuthContext.jsx
+++ servilink-pro/src/context/AuthContext.jsx
@@ -11,7 +11,7 @@ export const useAuth = () => {
   }
   return context;
 };
-
+ 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
